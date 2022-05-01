eba5682e916007cdcbeb0826bce7cdadceaac15b
noone <ahlijun1116@outlook.com>
2022-05-01 16:22:19 +0800

    add blogsome

--- src/main/java/com/noone/controller/BlogController.java
+++ src/main/java/com/noone/controller/BlogController.java
@@ -1,20 +1,73 @@
 package com.noone.controller;
 
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import cn.hutool.core.lang.Assert;
+import cn.hutool.core.util.ObjectUtil;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.noone.common.lang.Result;
+import com.noone.entity.Blog;
+import com.noone.service.BlogService;
+import com.noone.util.ShiroUtil;
+import org.apache.shiro.authz.annotation.RequiresAuthentication;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
 
-import org.springframework.web.bind.annotation.RestController;
+import java.time.LocalDateTime;
 
 /**
  * <p>
- *  前端控制器
+ * 前端控制器
  * </p>
  *
  * @author NoOne
  * @since 2022-04-29
  */
 @RestController
-@RequestMapping("/blog")
+
 public class BlogController {
+
+    @Autowired
+    BlogService blogService;
+
+    @RequestMapping("/blogs")
+    public Result list(@RequestParam(defaultValue = "1") Integer currentPage) {
+        Page page = new Page(currentPage, 5);
+        IPage pageData = blogService.page(page, new QueryWrapper<Blog>().orderByDesc("create"));
+        return Result.success(pageData);
+
+    }
+
+    @RequestMapping("/blogs/{id}")
+    public Result detail(@PathVariable(name = "id") Long id) {
+        Blog blog = blogService.getById(id);
+        Assert.notNull(blog, "该博客已被删除");
+
+        return Result.success(blog);
+    }
+
+    @RequiresAuthentication
+    @RequestMapping("/blogs/edit")
+    public Result edit(@Validated @RequestBody Blog blog) {
+        Blog temp = null;
+        if (ObjectUtil.isNotNull(blog.getId())) {
+            temp = blogService.getById(blog.getId());
+            Assert.isTrue(temp.getId().equals(ShiroUtil.getProfile().getId()), "没有权限");
+
+        } else {
+            temp = new Blog();
+            temp.setUserId(ShiroUtil.getProfile().getId());
+            temp.setCreated(LocalDateTime.now());
+            temp.setStatus(0);
+        }
+
+        BeanUtils.copyProperties(blog, temp, "id", "userId", "created", "status");
+        blogService.saveOrUpdate(temp);
+        return Result.success(null);
+    }
+
 
 }
